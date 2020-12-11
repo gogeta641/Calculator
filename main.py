8b680986b3b24bbfec3b5e224575caaf41e3c921
Karthik Ramachandran <karthik.gogeta@gmail.com>
2020-12-11 04:35:11 +0000

    Recursion

--- main.py
+++ main.py
@@ -25,23 +25,31 @@ operations = {
     "/": divide
     }
 
-num1 = int(input("What's the first number?: "))
+def calculator():
+    num1 = int(input("What's the first number?: "))
 
-for operand in operations:
-    print(operand)
+    for operand in operations:
+        print(operand)
 
-operand = input("Pick an operations from the line above: ")
-num2 = int(input("What's the second number?: "))
+    operand = input("Pick an operations from the line above: ")
+    num2 = int(input("What's the second number?: "))
 
-answer = operations[operand](num1, num2)
-print(f"{num1} {operand} {num2} = {answer}")
+    answer = operations[operand](num1, num2)
+    print(f"{num1} {operand} {num2} = {answer}")
 
-repeat = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to ext.: ")
+    repeat = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ")
 
-while repeat == "y":
-    operand = input("Pick another operation: ")
-    num3 = int(input("What's the next number?: "))
-    second_answer = operations[operand](answer, num3)
-    print(f"{answer} {operand} {num3} = {second_answer}")
-    answer = second_answer
-    repeat = input(f"Type 'y' to continue calculating with {second_answer}, or type 'n' to ext.: ")
+    if repeat == "n":
+        calculator()
+
+    while repeat == "y":
+        operand = input("Pick another operation: ")
+        num3 = int(input("What's the next number?: "))
+        second_answer = operations[operand](answer, num3)
+        print(f"{answer} {operand} {num3} = {second_answer}")
+        answer = second_answer
+        repeat = input(f"Type 'y' to continue calculating with {second_answer}, or type 'n' to start a new caculation.: ")
+        if repeat == "n":
+            calculator()
+
+calculator()
